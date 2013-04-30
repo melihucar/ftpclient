07e079b659363e3563035e129f3d8f29d4749a0c
guilex <dragan.stefanov@gmail.com>
2013-04-30 14:25:16 +0200

    modified structure to allow autoload

--- src/Melihucar/FtpClient/FtpClient.php
+++ src/Melihucar/FtpClient/FtpClient.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Melihucar\FtpClient;
 /**
  * FTP Client for PHP
  * 
@@ -18,7 +18,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
  * OR OTHER DEALINGS IN THE SOFTWARE.
  */
-class FTPClient
+class FtpClient
 {
 
     // Const variables
@@ -39,7 +39,7 @@ class FTPClient
      * 
      * Checks if ftp extension is loaded.
      */
-    public function FTPClient()
+    public function __construct()
     {
         if ( !extension_loaded('ftp') ) {
             throw new Exception('FTP extension is not loaded!');            
